3f2770614df2e7a9b8a3617cc5f9e250341df2fe
TheGreekCuban <theodorealexander92@gmaill.com>
2019-08-25 19:35:26 -0400

    server running

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const dotenv = require("dotenv").config()
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
@@ -8,21 +9,31 @@ const axios = require("axios")
 const cheerio = require("cheerio")
 
 // Require all models
-const db = require("./models")
+//const db = require("./models")
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3030
 
 // Initialize Express
 const app = express()
 
 // Configure middleware
 // Parse request body as JSON
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({
+    extended: true
+}))
 app.use(express.json())
+
 // Make public a static folder
 app.use(express.static("public"))
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrape-oholic"
 
 // Connect to the Mongo DB
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true
+})
+
+// Start the server
+app.listen(PORT, function () {
+    console.log("App listening on http://localhost:" + PORT);
+});
