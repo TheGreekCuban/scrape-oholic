ae0c2d35947b89611e8f464d1c7059f8b4e8cbb6
TheGreekCuban <theodorealexander92@gmaill.com>
2019-08-25 17:04:55 -0400

    added heroku, mongodb, folder structure 70%

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+
+const dotenv = require("dotenv").config()
+
+
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
