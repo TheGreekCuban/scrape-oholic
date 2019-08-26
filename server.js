b3d93e83c488015e0ff70812318aa90c8709b73a
TheGreekCuban <theodorealexander92@gmaill.com>
2019-08-26 14:45:49 -0400

    finished first get route but data not coming through

--- server.js
+++ server.js
@@ -2,16 +2,10 @@ const express = require("express")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv").config()
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-const axios = require("axios")
-const cheerio = require("cheerio")
 
-// Require all models
 //const db = require("./models")
 
-const PORT = process.env.PORT || 3030
+const PORT = process.env.PORT
 
 // Initialize Express
 const app = express()
@@ -25,6 +19,17 @@ app.use(express.json())
 
 // Make public a static folder
 app.use(express.static("public"))
+
+//Set handlebars
+const exphbs = require("express-handlebars")
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Import the routes and give the server access to them
+const routes = require("./controllers/scraper_controller")
+
+app.use(routes)
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrape-oholic"
 
